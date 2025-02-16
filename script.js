83c47abc18c5d690bc3d959da0f0817dbd2c5d53
Quick Tech <quicktech994@gmail.com>
2025-02-16 15:31:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const students = {
-    "283928": { password: "382828", name: "Deepak Kumar", formUrl: "" },
+    "225745": { password: "9A", name: "Piyush Raj", formUrl: "https://www.youtube.com/" },
     "622829": { password: "728919", name: "Aayushr", formUrl: "" },
     "205592": { password: "9A", name: "Ashutosh Sir", formUrl: "https://v0-thank-you-page-wsxxnpzuujw-ni5xqca0p-quicktech89s-projects.vercel.app/" },
   
